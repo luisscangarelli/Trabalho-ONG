0121188f4aab43512736257e52a8ed89f89a0a09
luisscangarelli <luis.scangarelli@soulasalle.com.br>
2025-06-04 06:41:23 -0300

    Estrutura inicial do projeto Flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sua-chave-secreta-aqui'
+
+# Configuração MySQL -
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://usuario:senha@localhost/nome_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
